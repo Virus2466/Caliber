b0ac7c8a0c4f4ff7a97a3face0dfb844dec5883b
Dev Patel <49341610+Virus2466@users.noreply.github.com>
2026-04-20 00:40:49 +0530

    [CLASS]  : Completed Buffer Class

---
+++ src/renderer/Buffer.cpp
@@ -0,0 +1,100 @@
+#include "renderer/Buffer.h"
+#include <cstdint>
+#include<utility>
+
+namespace Caliber{
+    
+    // --- VERTEX BUFFER ---
+    VertexBuffer::VertexBuffer(const float* data , uint32_t size){
+        glGenBuffers(1,&m_id);
+        glBindBuffer(GL_ARRAY_BUFFER , m_id);
+        glBufferData(GL_ARRAY_BUFFER , size , data , GL_STATIC_DRAW);
+    }
+
+    VertexBuffer::~VertexBuffer(){
+        if(m_id != 0){
+            glDeleteBuffers(1 , &m_id);
+        }
+    }
+
+    // move 
+    VertexBuffer::VertexBuffer(VertexBuffer&& other) noexcept 
+    : m_id(std::exchange(other.m_id , 0)) {}
+
+    VertexBuffer& VertexBuffer::operator=(VertexBuffer&& other) noexcept{
+        if(this != &other){
+            if(m_id != 0) glDeleteBuffers(1,&m_id);
+            m_id = std::exchange(other.m_id , 0);
+        }
+        return *this;
+    }
+
+
+
+
+    // --- INDEX BUFFER ---
+    IndexBuffer::IndexBuffer(const uint32_t* data , uint32_t count) : m_count(count) {
+        glGenBuffers(1,&m_id);
+        glBindBuffer(GL_ARRAY_BUFFER , m_id);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER , count * sizeof(uint32_t) , data , GL_STATIC_DRAW);
+    }
+
+    IndexBuffer::~IndexBuffer(){
+         if(m_id != 0){
+            glDeleteBuffers(1 , &m_id);
+        }
+    }
+
+    IndexBuffer::IndexBuffer(IndexBuffer&& other) noexcept 
+    : m_id(std::exchange(other.m_id, 0)),
+      m_count(std::exchange(other.m_count, 0)) {}
+
+
+    IndexBuffer& IndexBuffer::operator=(IndexBuffer&& other) noexcept {
+        if(this != &other){
+            if(m_id != 0) glDeleteBuffers(1,&m_id);
+            m_id = std::exchange(other.m_id , 0);
+            m_count = std::exchange(other.m_count , 0);
+
+        }
+        return  *this;
+    }
+
+
+
+    // --- VERTEX ARRAY ---
+    VertexArray::VertexArray() {
+        glGenVertexArrays(1,&m_id);
+    }
+
+    VertexArray::~VertexArray(){
+        if(m_id != 0){
+            glDeleteVertexArrays(1,&m_id);
+        }
+    }
+
+    VertexArray::VertexArray(VertexArray&& other) noexcept
+    : m_id(std::exchange(other.m_id , 0)){}
+
+    VertexArray& VertexArray::operator=(VertexArray&& other) noexcept{
+        if(this != &other){
+            if (m_id != 0) glDeleteVertexArrays(1,&m_id);
+            m_id = std::exchange(other.m_id , 0);
+        }
+        return *this;
+    }
+
+    void VertexArray::addAttribute(uint32_t index , int count , uint32_t stride ,uint32_t offset){
+        glad_glEnableVertexAttribArray(index);
+
+        /**
+            glVertexAttribPointer expects the offset as a const void* pointer — a C legacy thing from before modern OpenGL. But we have a plain integer offset.
+            reinterpret_cast says "take these bytes and pretend they are a different type" -- wierd ass function in my opinion :_
+        */
+        glVertexAttribPointer(index , count , GL_FLOAT , GL_FALSE , stride , reinterpret_cast<const void*>(offset));
+    }
+
+
+
+
+} // namespace caliber
--- src/renderer/Buffer.h
+++ src/renderer/Buffer.h
@@ -1,7 +1,14 @@
+/**
+ * @since 20th April 2026
+ * @author Dev Patel.
+ * -- Buffer Class to reduce the vertices that send to the GPU , that is important for our task that is handling large vertices.
+ * -- And it is also simplifying the main file.
+ */
+
+
 #pragma once 
 
 #include<cstdint>
-// #include<span>
 #include<glad/glad.h>
 
 
@@ -10,7 +17,7 @@ namespace Caliber {
     public:
         VertexBuffer(const float* data , uint32_t size);
         ~VertexBuffer();
-
+  
         // Deleting copy constructor.
         VertexBuffer(const VertexBuffer&) = delete;
         VertexBuffer& operator=(const VertexBuffer&) = delete;
@@ -27,6 +34,58 @@ namespace Caliber {
         uint32_t m_id{0};
     };
 
+    // Index Buffer Object -- 
+    class IndexBuffer{
+        public:
+            IndexBuffer(const uint32_t* data , uint32_t count);
+            
+            // Deleting Copy Constructor 
+            IndexBuffer(const IndexBuffer&) = delete;
+            IndexBuffer& operator=(const IndexBuffer&) = delete;
+
+            // Move assignment and move constructor.
+            IndexBuffer(IndexBuffer&& other) noexcept;
+            IndexBuffer& operator=(IndexBuffer&& other) noexcept;
+
+            void bind() const {glBindBuffer(GL_ELEMENT_ARRAY_BUFFER , m_id);}
+            void unbind() const {glBindBuffer(GL_ELEMENT_ARRAY_BUFFER , 0);}
+
+            [[nodiscard]] uint32_t getCount() const {return m_count;}
+            [[nodiscard]] uint32_t getID() const { return  m_id;}
+
+            // Destructor -- kek
+            ~IndexBuffer();
+
+        private:
+            uint32_t m_id{0};
+            uint32_t m_count {0};
+        
+    };
+
+    class VertexArray{
+        public:
+            VertexArray();
+            ~VertexArray();
+            
+            // No copyable
+            VertexArray(const VertexArray&) = delete;
+            VertexArray& operator=(const VertexArray&) = delete;
+
+            // Move 
+            VertexArray(VertexArray&& other) noexcept;
+            VertexArray& operator=(VertexArray&& other) noexcept;
+
+            void bind() const {glBindVertexArray(m_id);}
+            void unbind() const {glBindVertexArray(0);}
+
+            // we tell opengl how vertex data is designed ?????????????
+            void addAttribute(uint32_t index , int count , uint32_t stride ,uint32_t offset);
+
+            [[nodiscard]] uint32_t getID() const { return m_id;}
+
+        private:
+            uint32_t m_id{0};
+    };
 
 
 } // namespace caliber
