c7168064dbee1985dd5f2a7ab969fa4335510f8f
Dev Patel <49341610+Virus2466@users.noreply.github.com>
2026-05-26 12:35:37 +0530

    minort changes

--- src/main.cpp
+++ src/main.cpp
@@ -174,6 +174,7 @@ int main(){
             // clear BOTH buffers at the TOP
             glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            // Add polygon mode here if needed.
 
             // -------------- UI SECTION -----------------
             Caliber::UI::beginFrame();
